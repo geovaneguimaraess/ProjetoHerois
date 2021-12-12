6b3f960f979953d4c5d390f523842c2d04687ed8
geovaneguimaraess <geoguimaraes20@gmail.com>
2021-12-12 16:38:23 -0300

    Estrutura do projeto

---
+++ src/main/java/com/javadiotqi2/heroisMarvelDc/HeroismarveldcapiApplication.java
@@ -0,0 +1,14 @@
+package com.javadiotqi2.heroisMarvelDc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroismarveldcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroismarveldcapiApplication.class, args);
+		System.out.println("Herois Marvel e DC");
+	}
+
+}
